5d86f61f70dfca2571ac9462f4ce4efaf7741245
logicalmechanism <logical.mechanism@protonmail.com>
2023-07-11 10:09:17 -0700

    starting happy path

--- complete_build.sh
+++ complete_build.sh
@@ -32,7 +32,7 @@ pid_cbor=$(python ./convert_to_cbor.py ${pid})
 tkn=$(jq -r '.starterTkn' start_info.json)
 tkn_cbor=$(python ./convert_to_cbor.py ${tkn})
 
-echo -e "\033[1;33m Convert DAO Contract \033[0m"
+echo -e "\033[1;33m\nConvert DAO Contract \033[0m"
 aiken blueprint apply -o plutus.json -v dao.params "${pid_cbor}" .
 aiken blueprint apply -o plutus.json -v dao.params "${tkn_cbor}" .
 aiken blueprint convert -v dao.params > contracts/dao_contract.plutus
@@ -52,7 +52,7 @@ poolId=$(jq -r '.poolId' start_info.json)
 ran=$(jq -r '.random' start_info.json)
 ran_cbor=$(python ./convert_to_cbor.py ${ran})
 
-echo -e "\033[1;33m Convert Stake Contract \033[0m"
+echo -e "\033[1;33m\nConvert Stake Contract \033[0m"
 aiken blueprint apply -o plutus.json -v stake.params "${pid_cbor}" .
 aiken blueprint apply -o plutus.json -v stake.params "${tkn_cbor}" .
 aiken blueprint apply -o plutus.json -v stake.params "${dao_hash_cbor}" .
@@ -74,7 +74,7 @@ jq \
 stake_hash=$(cat hashes/stake_contract.hash)
 echo The Stake Contract Hash: ${stake_hash}
 
-echo -e "\033[1;33m Convert Mint Contract \033[0m"
+echo -e "\033[1;33m\nConvert Mint Contract \033[0m"
 aiken blueprint apply -o plutus.json -v mint.params "${pid_cbor}" .
 aiken blueprint apply -o plutus.json -v mint.params "${tkn_cbor}" .
 aiken blueprint apply -o plutus.json -v mint.params "${dao_hash_cbor}" .
@@ -85,7 +85,7 @@ mirror_policy=$(cat hashes/mint_contract.hash)
 echo The Mint Contract Hash: ${mirror_policy}
 mirror_policy_cbor=$(python ./convert_to_cbor.py ${mirror_policy})
 
-echo -e "\033[1;33m Convert Lock Contract \033[0m"
+echo -e "\033[1;33m\nConvert Lock Contract \033[0m"
 aiken blueprint apply -o plutus.json -v lock.params "${mirror_policy_cbor}" .
 aiken blueprint convert -v lock.params > contracts/lock_contract.plutus
 cardano-cli transaction policyid --script-file contracts/lock_contract.plutus > hashes/lock_contract.hash
@@ -93,7 +93,7 @@ cardano-cli transaction policyid --script-file contracts/lock_contract.plutus > 
 lock_hash=$(cat hashes/lock_contract.hash)
 echo The Lock Contract Hash: ${lock_hash}
 
-echo -e "\033[1;33m Convert Vault Contract \033[0m"
+echo -e "\033[1;33m\nConvert Vault Contract \033[0m"
 aiken blueprint apply -o plutus.json -v vault.params "${pid_cbor}" .
 aiken blueprint apply -o plutus.json -v vault.params "${tkn_cbor}" .
 aiken blueprint apply -o plutus.json -v vault.params "${dao_hash_cbor}" .
@@ -103,7 +103,7 @@ cardano-cli transaction policyid --script-file contracts/vault_contract.plutus >
 vault_hash=$(cat hashes/vault_contract.hash)
 echo The Vault Contract Hash: ${vault_hash}
 
-echo -e "\033[1;33m Convert Threshold NFT Contract \033[0m"
+echo -e "\033[1;33m\nConvert Threshold NFT Contract \033[0m"
 aiken blueprint apply -o plutus.json -v nft.params "${pid_cbor}" .
 aiken blueprint apply -o plutus.json -v nft.params "${tkn_cbor}" .
 aiken blueprint apply -o plutus.json -v nft.params "${dao_hash_cbor}" .
@@ -113,6 +113,16 @@ cardano-cli transaction policyid --script-file contracts/nft_contract.plutus > h
 
 nft_policy=$(cat hashes/nft_contract.hash)
 echo The NFT Contract Hash: ${nft_policy}
+
+echo -e "\033[1;33m\nConvert Mediation Contract \033[0m"
+aiken blueprint apply -o plutus.json -v mediator.params "${mirror_policy_cbor}" .
+aiken blueprint convert -v lock.params > contracts/mediator_contract.plutus
+cardano-cli transaction policyid --script-file contracts/mediator_contract.plutus > hashes/mediator_contract.hash
+
+mediator_hash=$(cat hashes/mediator_contract.hash)
+echo The Mediator Contract Hash: ${mediator_hash}
+
+# update datum information
 
 jq -r \
 --arg poolId "$poolId" \
